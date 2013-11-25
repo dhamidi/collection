8ed848e4df3505a15e94d98456c0807e95f59c7b
Dario Hamidi <dario.hamidi@xing.com>
2013-11-25 20:12:28 +0100

    Add tests and documentation.

--- collection.go
+++ collection.go
@@ -4,32 +4,73 @@ import (
 	"fmt"
 )
 
+// Value is an alias for the empty interface type.  This alias is
+// provided to make function signatures more readable.
 type Value interface{}
+
+// UnaryFunction encodes the type of callbacks expected by Map and MapX
 type UnaryFunction func(Value) Value
+
+// UnaryPredicate encodes the type of callbacks expected by Filter
 type UnaryPredicate func(Value) bool
+
+// BinaryFunction encodes the type of callbacks expected by Reduce and ReduceFirst
 type BinaryFunction func(Value, Value) Value
 
+// Collection is the interface that wraps a generic container with
+// indexed access to each element.
+//
+// Item returns a single element of the collection.  The element
+// is identified by `index'.
+//
+// SetItem sets a single element of the collection, identified
+// by `index', to a new value, `value'.  It returns the value of
+// the element previously identified by `index'.
+//
+// Length returns the number of elements in a collection.
+//
+// Append adds one element, `item', to the end of the
+// collection.  It returns a collection containing all elements
+// before the call to `Append' and `item'.  Whether the returned
+// collection is newly allocated or the same reference as the
+// method receiver is left up to implementation.
+//
+// Empty returns a new, empty collection of the same type.  This is
+// required for supporting `Filter' and a non-destructive `Map'.
 type Collection interface {
 	Item(index int) Value
 	SetItem(index int, value Value) Value
 	Length() int
 	Append(item Value) Collection
+	Empty() Collection
 }
 
-type Vector []Value
+// Vector is a collection based on Go's builtin slice type.
+type Vector struct {
+	data []Value
+}
 
-func NewVector(items []Value) Vector {
+// NewVector creates a new vector initially containing the elements
+// passed in `items'.
+//
+// The value of `items' may be `nil', in which case a new slice of
+// length 0 is allocated and used instead.
+func NewVector(items []Value) *Vector {
 	data := items
 
 	if items == nil {
-		data = []Value{}
+		data = make([]Value, 0)
 	}
 
-	return data
+	return &Vector{data: data}
 }
 
-func NewVectorInt(items []int) Vector {
-	result := Vector{}
+// NewVectorInt works like NewVector, except that it accepts a slice of
+// `int'.  The resulting vector is built using `Append'.
+func NewVectorInt(items []int) *Vector {
+	result := &Vector{
+		data: make([]Value, 0),
+	}
 
 	if items == nil {
 		return result
@@ -42,32 +83,59 @@ func NewVectorInt(items []int) Vector {
 	return result
 }
 
-func (self Vector) String() string {
-	return fmt.Sprintf("%v", self)
+// String returns the string representation of a vector.  This matches
+// Go's `v' format specifier of the underlying slice.
+func (self *Vector) String() string {
+	return fmt.Sprintf("%v", self.data)
 }
 
-func (self Vector) Length() int {
-	return len(self)
+// Length returns the length of the vector.  This is equal to calling
+// Go's built-in `len' function on the underlying slice.
+func (self *Vector) Length() int {
+	return len(self.data)
 }
 
-func (self Vector) Item(index int) Value {
-	return self[index]
+// Item returns the element located at position `index'.  This is equal
+// to indexing the underlying slice using brackets.  If the index is out
+// of bounds, this function panics.
+func (self *Vector) Item(index int) Value {
+	return self.data[index]
 }
 
-func (self Vector) SetItem(index int, value Value) Value {
+// SetItem sets the element at position `index' to `value' and returns
+// the element that was at position `index' before the assignment.
+func (self *Vector) SetItem(index int, value Value) Value {
 	oldValue := self.Item(index)
-	self[index] = value
+
+	self.data[index] = value
+
 	return oldValue
 }
 
-func (self Vector) Append(item Value) Collection {
-	self = append(self, item)
+// Append appends `item' to the end of the underlying slice and returns
+// the method receiver.
+func (self *Vector) Append(item Value) Collection {
+	self.data = append(self.data, item)
+
 	return self
 }
 
+// Empty returns a new, empty vector.  The capacity of the vector is
+// initialized to the `Length' of the method receiver.
+func (self *Vector) Empty() Collection {
+	return NewVector(make([]Value, 0, self.Length()))
+}
+
+
+// Map determines the length of the collection `c' exactly once and then
+// iterates over the range [0, length).
+//
+// It returns a new collection that contains the results of applying `f'
+// to each element in that range of the original collection.
 func Map(c Collection, f UnaryFunction) Collection {
-	results := NewVector(nil)
+	results := c.Empty()
 	len := c.Length()
+
 	for index := 0; index < len; index++ {
 		results.Append(f(c.Item(index)))
 	}
@@ -75,6 +143,8 @@ func Map(c Collection, f UnaryFunction) Collection {
 	return results
 }
 
+// MapX works like `Map', except that it modifies and returns the
+// original collection (`c').
 func MapX(c Collection, f UnaryFunction) Collection {
 	len := c.Length()
 
@@ -86,17 +156,43 @@ func MapX(c Collection, f UnaryFunction) Collection {
 	return c
 }
 
-func Reduce(c Collection, f BinaryFunction) Value {
-	result := c.Item(0)
+// Reduce applies a function of two arguments `f' to an accumulator
+// value and successive elements of `c'.  The length of `c' is
+// determined once using `Length'.  It returns the value of the
+// accumulator at the end of the iteration.
+//
+// The first argument to `f' is the accumulator (initialized to
+// `initial'), the second argument is the current element of the
+// collection.
+func Reduce(c Collection, f BinaryFunction, initial Value) Value {
+	result := initial
 	len := c.Length()
-	for index := 1; index < len; index++ {
+
+	for index := 0; index < len; index++ {
 		result = f(result, c.Item(index))
 	}
+
 	return result
 }
 
+// ReduceFirst works like `Reduce', except that it uses the first
+// element of the collection as the initial value.
+func ReduceFirst(c Collection, f BinaryFunction) Value {
+	result := c.Item(0)
+	len := c.Length()
+
+	for index := 1; index < len; index++ {
+		result = f(result, c.Item(index))
+	}
+
+	return result
+}
+
+
+// Filter returns a new collection containing only the elements of `c'
+// for which `p' returns `true'.
 func Filter(c Collection, p UnaryPredicate) Collection {
-	result := NewVector(nil)
+	result := c.Empty()
 	len := c.Length()
 	var item Value
 
--- collection_test.go
+++ collection_test.go
@@ -2,19 +2,106 @@ package collection
 
 import (
 	"testing"
+	"fmt"
 )
 
+// doubleInt is used as a UnaryFunction for testing Map and MapX
+func doubleInt(i Value) Value {
+	return i.(int) * 2
+}
+
+// addInt is used as a BinaryFunction for testing Reduce and ReduceFirst
+func addInt(a, b Value) Value {
+	return a.(int) + b.(int)
+}
+
+// isEvenInt is used as a UnaryPredicate for testing Filter and sort
+func isEvenInt(a Value) bool {
+	return a.(int) % 2 == 0
+}
+
+func TestVector_Length(t *testing.T) {
+	v := NewVectorInt([]int{1,2,3})
+
+	if length := v.Length(); length != 3 {
+		t.Errorf("Expected vector.Length() to be %d, got %d", 3, length)
+	}
+}
+
+func TestVector_Append(t *testing.T) {
+	v := NewVector(nil)
+	v.Append(1)
+
+	if length := v.Length(); length != 1 {
+		t.Errorf("Expected vector.Append(1) to increase length by one")
+	}
+
+	if item := v.Item(0); item.(int) != 1 {
+		t.Errorf("Expected the first item to be 1, got %v", item)
+	}
+}
+
+func TestVector_Item(t *testing.T) {
+	v := NewVectorInt([]int{1,2,3})
+
+	length := v.Length()
+	for index := 0; index < length; index++ {
+		if item := v.Item(index); item.(int) != index+1 {
+			t.Errorf("Expected vector.Item(%d) to be %d, got %v", index, index+1, item)
+		}
+	}
+}
+
+func TestVector_SetItem(t *testing.T) {
+	v := NewVector(nil)
+	v.Append(1)
+	v.SetItem(0, 2)
+
+	if actual, expected := v.Item(0), 2; actual != expected {
+		t.Errorf("Expected v.Item(0) to be %v, got %v", expected, actual)
+	}
+}
+
+func TestVector_String(t *testing.T) {
+	v := NewVectorInt([]int{1,2,3})
+
+	if actual, expected := v.String(), "[1 2 3]"; actual != expected {
+		t.Errorf("Expected v.String() to be %v, got %v", expected, actual)
+	}
+}
+
+func TestMap(t *testing.T) {
+	v := NewVectorInt([]int{1,2,3})
+	mv := Map(v, doubleInt)
+	len := v.Length()
+
+	for index := 0; index < len; index++ {
+		if mv.Item(index) != 2 * v.Item(index).(int) {
+			t.Errorf("Expected mappedVector[%d] to be %d", index, 2 * v.Item(index).(int))
+		}
+	}
+}
+
 func ExampleMap() {
-	v := NewVectorInt([]int{2, 4, 7})
+	v := NewVectorInt([]int{1,2,3})
 
 	r := Map(v, func(i Value) Value {
 		return i.(int) * 2
 	})
+	fmt.Printf("%v\n", r)
+	// Output: [2 4 6]
+}
 
-	fmt.Printf("Numbers: %v\n", r)
+func TestMapX(t *testing.T) {
+	v := NewVectorInt([]int{1,2,3})
+	mv := MapX(v, doubleInt)
+	len := v.Length()
 
-	// Output:
-	// Numbers: [4 8 14]
+	for index := 0; index < len; index++ {
+		if actual, expected := mv.Item(index), v.Item(index); actual != expected {
+			t.Errorf("Expected mv.Item(index) to be %v, got %v", expected, actual)
+		}
+	}
 }
 
 func ExampleMapX() {
@@ -31,11 +118,19 @@ func ExampleMapX() {
 	// r: [4 8 12]
 }
 
+func TestReduce(t *testing.T) {
+	v := NewVectorInt([]int{2,4,6})
+	sum := Reduce(v, addInt, 0)
+	if actual, expected := sum, 12; actual != expected {
+		t.Errorf("Expected sum to be %v, got %v", expected, actual)
+	}
+}
+
 func ExampleReduce() {
 	r := NewVectorInt([]int{1,2,3})
 	s := Reduce(r, func(i, j Value) Value {
 		return i.(int) + j.(int)
-	})
+	}, 0)
 
 	fmt.Printf("Sum: %v\n", s)
 
@@ -43,14 +138,45 @@ func ExampleReduce() {
 	// Sum: 6
 }
 
+func TestReduceFirst(t *testing.T) {
+	v := NewVectorInt([]int{1,2,3})
+	if actual, expected := ReduceFirst(v, addInt), 6; actual != expected {
+		t.Errorf("Expected ReduceFirst(v, addInt) to be %v, got %v", expected, actual)
+	}
+}
+
+func ExampleReduceFirst() {
+	r := NewVectorInt([]int{1,2,3})
+	s := ReduceFirst(r, func(i, j Value) Value {
+		return i.(int) + j.(int)
+	})
+
+	fmt.Printf("Sum: %v\n", s)
+	// Output:
+	// Sum: 6
+}
+
+func TestFilter(t *testing.T) {
+	v := NewVectorInt([]int{1,2,3})
+	filtered := Filter(v, isEvenInt)
+
+	if actual, expected := filtered.Item(0), 2; actual != expected {
+		t.Errorf("Expected filtered.Item(0) to be %v, got %v", expected, actual)
+	}
+
+	if actual, expected := filtered.Length(), 1; actual != expected {
+		t.Errorf("Expected filtered.Length() to be %v, got %v", expected, actual)
+	}
+}
+
 func ExampleFilter() {
 	r := NewVectorInt([]int{1,2,3})
 
-	even := func(i interface{}) bool {
+	even := func(i Value) bool {
 		return i.(int) % 2 == 0
 	}
 
-	evenNumbers := Filter(v, even)
+	evenNumbers := Filter(r, even)
 
 	fmt.Printf("Even numbers: %v\n", evenNumbers)
 
